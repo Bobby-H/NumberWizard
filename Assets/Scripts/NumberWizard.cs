1c0ec5138bf7737976a1882bef874fe19c341bad
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-07-27 11:36:40 -0400

    initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+		
+	void Start(){
+		StartGame();
+	}
+	
+	void StartGame(){
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
+		max = max +1;
+		
+		print ("===========================");
+		print ("Welcome to Number Wizard!");
+		print ("pick a number in your head, but don't tell me!");
+		
+		print ("The highest number you can pick is " + max);
+		print ("The highest number you can pick is " + min);
+		
+		print ("Is the number higher or lower than "+ guess +"?");
+		print ("Up arrow for higher, down arrow for lower, return if I guess it!");
+	}
+	
+	void NextGuess(){
+		guess = (max + min)/2;
+		print("Higher or Lower than" + guess + "?");
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			min = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I won!");
+			StartGame();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
